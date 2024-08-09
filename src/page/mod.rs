cf201a092b1d5cc03944e230ad20106cfa460d89
Yogi Astawan <yogi.astawan@gmail.com>
2024-08-09 14:23:18 +0800

    Add function create_panel

--- src/page/mod.rs
+++ src/page/mod.rs
@@ -31,17 +31,35 @@ impl Page {
     pub(crate) fn set_side_bar(&self, panel: Option<SidePanel>) {
         *self.left_side.borrow_mut() = panel;
     }
+
+    fn create_panel(&self) {
+        let ctn = self.md.borrow();
+        let side_panel = if let Some(x) = ctn.as_ref() {
+            Some(x.create_side_panel())
+        } else {
+            None
+        };
+
+        self.set_side_bar(side_panel);
+    }
 }
 
 impl Renderer for Page {
     fn render(&self) -> String {
         let side_bar = self.left_side.borrow();
+
+        if let None = side_bar.as_ref() {
+            self.create_panel();
+        }
+
+        let side_bar = self.left_side.borrow();
+
         let side_bar = match side_bar.as_ref() {
             Some(x) => x.render(),
-            None => String::from(""),
+            None => String::new(),
         };
 
-        let contn = self.left_side.borrow();
+        let contn = self.md.borrow();
         let content = match contn.as_ref() {
             Some(x) => x.render(),
             None => String::from(""),
