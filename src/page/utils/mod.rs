d0185de14e11678bdb7da6aa84745eac3f8e1fdc
Yogi Astawan <yogi.astawan@gmail.com>
2024-08-09 13:52:36 +0800

    Remove unused struct

--- src/page/utils/mod.rs
+++ src/page/utils/mod.rs
@@ -1,12 +1,2 @@
-use std::cell::RefCell;
-
-use content::Content;
-use side_panel::SidePanel;
-
 pub(crate) mod content;
 pub(crate) mod side_panel;
-
-pub(crate) struct Page {
-    content: RefCell<Content>,
-    side_panel: RefCell<SidePanel>,
-}
